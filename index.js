fa5088b68eb6fa68faac64355dc4c7f73ab183f1
GK3077 <gayathrikrishnan.gayu@gmail.com>
2023-01-25 11:34:35 +0530

    Added additional features

--- index.js
+++ index.js
@@ -4,17 +4,30 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame(){
+    renderGame()
+}
+
+function renderGame(){
+    sumEl.textContent = "Sum: "+ sum
+    cardsEl.textContent =  "Cards: " + firstCard + " " + secondCard
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card? "
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "Wohoo! You've got Blackjack! "
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game! "
         isAlive = false
     }
-    console.log(message)
+    messageEl.textContent = message
+}
+function newCard(){
+    let card = 7
+    sum += card
+    renderGame()
 }
