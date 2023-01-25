0bf0e107dbeb01cbe4e3ef74b0a58d4339ed941e
GK3077 <gayathrikrishnan.gayu@gmail.com>
2023-01-25 13:00:29 +0530

    Added randomness and player name and chips

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+let player = {
+    name : "GK",
+    chips : 200
+}
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random()*13) +1
+    if(randomNumber >10){
+        return 10
+    }
+    else if (randomNumber === 1){
+        return 11
+    }
+    else{
+        return randomNumber
+    }
+}
 
 function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
 function renderGame(){
+    cardsEl.textContent =  "Cards: " 
+
+    for( i = 0; i < cards.length; i++ ){
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = "Sum: "+ sum
-    cardsEl.textContent =  "Cards: " + firstCard + " " + secondCard
+    
     if (sum <= 20) {
         message = "Do you want to draw a new card? "
     } else if (sum === 21) {
@@ -27,7 +57,11 @@ function renderGame(){
     messageEl.textContent = message
 }
 function newCard(){
-    let card = 7
-    sum += card
-    renderGame()
+    if (isAlive && !hasBlackJack){
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+    
 }
